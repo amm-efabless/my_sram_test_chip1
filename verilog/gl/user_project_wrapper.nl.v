fea7c26010dba77c0e3fc023ce52ae8737997e94
Anton Maurovic <anton.maurovic@efabless.com>
2024-05-09 18:28:39 +0930

    Updating UPW. Adding GL netlists

--- verilog/gl/user_project_wrapper.nl.v
+++ verilog/gl/user_project_wrapper.nl.v
@@ -1,12 +1,5 @@
+// This is the unpowered netlist.
 module user_project_wrapper (user_clock2,
-    vccd1,
-    vccd2,
-    vdda1,
-    vdda2,
-    vssa1,
-    vssa2,
-    vssd1,
-    vssd2,
     wb_clk_i,
     wb_rst_i,
     wbs_ack_o,
@@ -26,14 +19,6 @@ module user_project_wrapper (user_clock2,
     wbs_dat_o,
     wbs_sel_i);
  input user_clock2;
- input vccd1;
- input vccd2;
- input vdda1;
- input vdda2;
- input vssa1;
- input vssa2;
- input vssd1;
- input vssd2;
  input wb_clk_i;
  input wb_rst_i;
  output wbs_ack_o;
@@ -55,8 +40,6 @@ module user_project_wrapper (user_clock2,
 
 
  wishbone_sram mprj (.sram_selected(io_out[37]),
-    .vccd1(vccd1),
-    .vssd1(vssd1),
     .wb_clk_i(wb_clk_i),
     .wb_rst_i(wb_rst_i),
     .wbs_ack_o(wbs_ack_o),
@@ -164,3 +147,4 @@ module user_project_wrapper (user_clock2,
     wbs_sel_i[1],
     wbs_sel_i[0]}));
 endmodule
+
